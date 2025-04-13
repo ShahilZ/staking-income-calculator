1fd34876acd966a9a954d98cfc1b6acf5895d002
Shahil Zhangada <shahilgreenz@yahoo.com>
2025-04-13 12:54:30 -0700

    Support cosmos via mintscan's csv

--- cmd.py
+++ cmd.py
@@ -3,7 +3,7 @@ import asyncio
 import logging
 
 from protocol import Protocol
-from calculator import calculate_staking_income
+from calculator import calculate_staking_income, calculate_staking_income_without_daily_prices
 
 # Configure logging with a custom format
 logging.basicConfig(
@@ -15,19 +15,22 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 @click.command()
-@click.option('--year', '-y', type=int, required=True, help='Tax year for which to calculate the staking income.')
-@click.option('--protocol', '-p', type=str, required=True, help='Staking protocol.')
-@click.option('--address', '-a', type=str, required=False, help='Address of the staking wallet.')
-@click.option('--reward-file', '-r', type=str, required=False, help='File containing the reward data.')
-def main(year, protocol, address, reward_file):
+@click.option('--year', type=int, required=True, help='Tax year for which to calculate the staking income.')
+@click.option('--protocol', type=str, required=True, help='Staking protocol.')
+@click.option('--address', type=str, required=False, help='Address of the staking wallet.')
+@click.option('--reward-file', type=str, required=False, help='File containing the reward data.')
+@click.option('--no-prices', is_flag=True, required=False, help='Calculate staking income without fetching daily prices.')
+def main(year, protocol, address, reward_file, no_prices):
     """A simple command-line tool using Click."""
     protocol = Protocol.from_name(protocol)
     if not address and not reward_file:
         raise ValueError("Either address or reward-file must be provided.")
     logger.info(f"Calculating {protocol} staking income for {address} for {year}.")
     
-    # Run the async function from synchronous code
-    staking_income = asyncio.run(calculate_staking_income(protocol, year, address, reward_file))
+    if no_prices:
+        staking_income = calculate_staking_income_without_daily_prices(protocol, year, address, reward_file)
+    else:
+        staking_income = calculate_staking_income(protocol, year, address, reward_file)
 
     # Sum the total tokens and usd amount.
     total_tokens = sum(reward.amount for reward in staking_income)
